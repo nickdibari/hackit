cefb8734dd9af994a3443e8afddff70a1d4637ef
nickdibari <ndibari@fordham.edu>
2020-02-26 01:10:59 -0500

    Add user with password from darkweb list

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def login():
 def login_endpoint():
     # Database of username/password combos
     users = {
-        'jsmith': 'soccer',     # common_passwords.txt
-        'aadams': 'yankees',    # common_passwords.txt
-        'vhedie': 'spiderman1'  # myspace.txt
+        'jsmith': 'soccer',         # common_passwords.txt
+        'aadams': 'lebron23',       # darkweb.txt
+        'vhedie': 'ungovernable',   # myspace.txt
     }
 
     username = request.form.get('username')
